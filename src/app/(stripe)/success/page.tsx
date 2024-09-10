f502a9808c627eca0274f3868232a7999ed86887
Florian Hacqueville-Mathi <florian.hacqueville@hotmail.fr>
2024-09-10 18:07:31 +0200

    fix

--- src/app/(stripe)/success/page.tsx
+++ src/app/(stripe)/success/page.tsx
@@ -1,20 +1,11 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import { useSearchParams } from 'next/navigation';
 import axios from 'axios';
 
-function CustomerData({ token }: { token: string }) {
-  const [customerData, setCustomerData] = useState<any>(null);
-  useEffect(() => {
-    if (token) {
-      axios
-        .get(`/api/success`, { params: { token } })
-        .then((response) => setCustomerData(response.data))
-        .catch((error) => console.error('Error fetching customer data:', error));
-    }
-  }, [token]);
-  if (!customerData) {return <p>Loading customer data...</p>;}
+function CustomerData({ customerData }: { customerData: any }) {
+  if (!customerData) {
+    return <p>Loading customer data...</p>;
+  }
   return (
     <div>
       <p>Merci pour votre achat, {customerData?.name}!</p>
@@ -25,14 +16,37 @@ function CustomerData({ token }: { token: string }) {
   );
 }
 
-export default function SuccessPage() {
-  const searchParams = useSearchParams();
-  const token = searchParams.get('token');
-  if (!token) { return <div>Loading...</div>;}
+export default function SuccessPage({ customerData }: { customerData: any }) {
   return (
     <div className='w-full h-screen flex items-center justify-center flex-col gap-3 text-center'>
       <h1>✅ Paiement réussi!</h1>
-      <CustomerData token={token} />
+      <CustomerData customerData={customerData} />
     </div>
   );
+}
+
+export async function getServerSideProps(context: any) {
+  const { token } = context.query;
+  if (!token) {
+    return {
+      notFound: true,
+    };
+  }
+  try {
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/success`, {
+      params: { token },
+    });
+    return {
+      props: {
+        customerData: res.data || null,
+      },
+    };
+  } catch (error) {
+    console.error('Error fetching customer data:', error);
+    return {
+      props: {
+        customerData: null,
+      },
+    };
+  }
 }
