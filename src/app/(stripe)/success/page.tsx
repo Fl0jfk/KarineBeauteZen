6f8a61be4206c0cf79f939aa1aa5579a4d0b8271
Florian Hacqueville-Mathi <florian.hacqueville@hotmail.fr>
2024-09-10 18:02:44 +0200

    test

--- src/app/(stripe)/success/page.tsx
+++ src/app/(stripe)/success/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState, Suspense } from 'react';
+import { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import axios from 'axios';
 
@@ -8,14 +8,13 @@ function CustomerData({ token }: { token: string }) {
   const [customerData, setCustomerData] = useState<any>(null);
   useEffect(() => {
     if (token) {
-      axios.get(`/api/success`, { params: { token } })
-        .then(response => setCustomerData(response.data))
-        .catch(error => console.error('Error fetching customer data:', error));
+      axios
+        .get(`/api/success`, { params: { token } })
+        .then((response) => setCustomerData(response.data))
+        .catch((error) => console.error('Error fetching customer data:', error));
     }
   }, [token]);
-  if (!customerData) {
-    return <p>Loading customer data...</p>;
-  }
+  if (!customerData) {return <p>Loading customer data...</p>;}
   return (
     <div>
       <p>Merci pour votre achat, {customerData?.name}!</p>
@@ -29,16 +28,11 @@ function CustomerData({ token }: { token: string }) {
 export default function SuccessPage() {
   const searchParams = useSearchParams();
   const token = searchParams.get('token');
-  if (!token) {
-    return <div>Loading...</div>;
-  }
+  if (!token) { return <div>Loading...</div>;}
   return (
     <div className='w-full h-screen flex items-center justify-center flex-col gap-3 text-center'>
       <h1>✅ Paiement réussi!</h1>
-      <Suspense fallback={<p>Loading customer data...</p>}>
-        <CustomerData token={token} />
-      </Suspense>
+      <CustomerData token={token} />
     </div>
   );
 }
-
