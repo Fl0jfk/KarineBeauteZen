d02aeb406167f339daf91208c37b8c18e3f57fa9
Florian Hacqueville-Mathi <florian.hacqueville@hotmail.fr>
2024-09-17 17:58:05 +0200

    fix slider

--- src/app/components/List/BrandList.tsx
+++ src/app/components/List/BrandList.tsx
@@ -19,11 +19,11 @@ export default function BrandList() {
     };
   }, []);
   return (
-    <section className="w-full p-4 max-w-[1200px] mx-auto flex flex-col gap-4 items-center">
+    <section className="w-full p-4 max-w-[1200px] mx-auto flex flex-col gap-8 items-center">
       <h2 className="text-4xl">Nos Marques partenaires</h2>
       <div ref={containerRef} className="bg-white w-full h-[350px] rounded-xl overflow-hidden md:h-[250px] sm:h-[200px]">
         {brands.map((brand) => (
-          <Image src={brand.logo} key={brand.id} alt={`Logo de la marque ${brand.name}`} width={800} height={800} className="w-full h-full object-contain"/>
+          <Image src={brand.logo} key={brand.id} alt={`Logo de la marque ${brand.name}`} width={800} height={800} className="w-full h-full"/>
         ))}
       </div>
     </section>
