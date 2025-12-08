f41e46ecd20135508b57a8704dc6630872ecb4e2
Florian Hacqueville-Mathif <f.hacqueville@OGEC-PROVIDENCE.LAN>
2025-12-08 16:37:41 +0100

    fix

--- src/app/(stripe)/success/page.tsx
+++ src/app/(stripe)/success/page.tsx
@@ -1,5 +1,11 @@
+// src/app/(stripe)/success/page.tsx
+import { Suspense } from "react";
 import SuccessContent from "./SuccessContent";
 
 export default function SuccessPage() {
-  return <SuccessContent />;
+  return (
+    <Suspense fallback={<div>Chargement...</div>}>
+      <SuccessContent />
+    </Suspense>
+  );
 }
